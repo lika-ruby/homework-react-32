079f86a6488979a7de750587fee68103e9946cd9
lika-ruby <mary.havrylova.2011@gmail.com>
2026-03-07 09:57:54 +0200

    makes all

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -17,6 +17,7 @@ const contactsSlice = createSlice({
     reducers: {
         addContact: {
             reducer(state, action) {
+                if (!state.contacts) state.contacts = [];
                 state.contacts.push(action.payload);
             },
             prepare(name, number) {
@@ -30,11 +31,13 @@ const contactsSlice = createSlice({
             },
         },
         deleteContact(state, action) {
+            if (!state.contacts) state.contacts = [];
             const contact = state.contacts.find(c => c.id === action.payload);
             state.contacts = state.contacts.filter(c => c.id !== action.payload);
             state.lastDeletedContact = contact || null;
         },
         restoreContact(state) {
+            if (!state.contacts) state.contacts = [];
             if (state.lastDeletedContact) {
                 state.contacts.push(state.lastDeletedContact);
                 state.lastDeletedContact = null;
@@ -42,6 +45,5 @@ const contactsSlice = createSlice({
         },
     },
 });
-
 export const { addContact, deleteContact, restoreContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
