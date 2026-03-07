3c7d3e6cf564fd0fa12b208df74eb55b60141696
lika-ruby <mary.havrylova.2011@gmail.com>
2026-03-07 10:11:36 +0200

    makes all

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
